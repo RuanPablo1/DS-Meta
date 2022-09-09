520013d7e5db93fc6287c705e9d5986a073a1a9d
Zaxyn <ruan.pablo2002@outlook.com.br>
2022-09-09 16:59:56 -0300

    Envio de notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        console.log("SUCESSO");
+    });
+}
+
+function NotificationButton({saleId}: Props) {
     return (
-        <div className="ds-meta-red-button">
+        <div className="ds-meta-red-button" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="notificar"/>
         </div>
     )
